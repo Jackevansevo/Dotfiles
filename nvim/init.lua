971c5a1b0b7e29d26f502b930669df5c5064d5fc
Jack Evans <jack@evans.gb.net>
2024-01-14 10:59:48 +0000

    wip

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,122 +1,24 @@
-vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-gitsigns_config = {
-  on_attach = function(bufnr)
-    local gs = package.loaded.gitsigns
-
-    local function map(mode, l, r, opts)
-      opts = opts or {}
-      opts.buffer = bufnr
-      vim.keymap.set(mode, l, r, opts)
-    end
-
-    -- Navigation
-    map('n', ']c', function()
-      if vim.wo.diff then return ']c' end
-      vim.schedule(function() gs.next_hunk() end)
-      return '<Ignore>'
-    end, {expr=true})
-
-    map('n', '[c', function()
-      if vim.wo.diff then return '[c' end
-      vim.schedule(function() gs.prev_hunk() end)
-      return '<Ignore>'
-    end, {expr=true})
-
-    -- Actions
-    map('n', '<leader>hs', gs.stage_hunk)
-    map('n', '<leader>hr', gs.reset_hunk)
-    map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-    map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-    map('n', '<leader>hS', gs.stage_buffer)
-    map('n', '<leader>hu', gs.undo_stage_hunk)
-    map('n', '<leader>hR', gs.reset_buffer)
-    map('n', '<leader>hp', gs.preview_hunk)
-    map('n', '<leader>hb', function() gs.blame_line{full=true} end)
-    map('n', '<leader>tb', gs.toggle_current_line_blame)
-    map('n', '<leader>hd', gs.diffthis)
-    map('n', '<leader>hD', function() gs.diffthis('~') end)
-    map('n', '<leader>td', gs.toggle_deleted)
-
-    -- Text object
-    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-  end
-}
-
-require("lazy").setup({
-  {
-    "lewis6991/gitsigns.nvim",
-    opts = gitsigns_config,
-  },
-  "neovim/nvim-lspconfig",
-  "nvim-treesitter/nvim-treesitter",
-  "radenling/vim-dispatch-neovim",
-  "github/copilot.vim",
-  "tartansandal/vim-compiler-pytest",
-  "tpope/vim-abolish",
-  "tpope/vim-dispatch",
-  "tpope/vim-fireplace",
-  "tpope/vim-fugitive",
-  "tpope/vim-rhubarb",
-  "bakpakin/janet.vim",
-  { 'echasnovski/mini.nvim', version = false },
-})
-
-require('mini.ai').setup({})
-require('mini.align').setup({})
-require('mini.basics').setup({
-  options = {
-    basic = true,
-    extra_ui = true,
-    win_borders = 'single',
-  }
-})
-require('mini.bracketed').setup({})
-require('mini.bufremove').setup({})
-require('mini.comment').setup({})
-require('mini.completion').setup({})
-require('mini.cursorword').setup({})
-require('mini.files').setup({})
-require('mini.jump').setup({})
-require('mini.jump2d').setup({})
-require('mini.move').setup({})
-require('mini.operators').setup({})
-require('mini.pairs').setup({})
-require('mini.pick').setup({})
-require('mini.sessions').setup({})
-require('mini.splitjoin').setup({})
-require('mini.starter').setup({})
-require('mini.statusline').setup({ set_vim_settings = false })
-require('mini.surround').setup({})
-require('mini.tabline').setup({})
-require('mini.trailspace').setup({})
-
 vim.opt.exrc = true
-vim.opt.number = false
+
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+
 vim.opt.laststatus = 3
-vim.cmd[[ colorscheme base16-tomorrow ]]
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.wrap = false
+
+vim.opt.background = 'light'
+vim.cmd.colorscheme "wildcharm"
+vim.cmd [[ :set statusline=%<%f\ %h%m%r%{FugitiveStatusline()}%=%-14.(%l,%c%V%)\ %P ]]
+vim.opt.winbar = "%n) %t"
 
 vim.cmd ([[ set grepprg=rg\ --vimgrep\ --no-heading\ --smart-case ]])
-
-vim.cmd([[
-    let g:loaded_ruby_provider = 0
-    let g:loaded_node_provider = 0
-    let g:loaded_perl_provider = 0
-    let g:python3_host_prog = '$PYENV_ROOT/versions/py3nvim/bin/python'
-]])
 
 vim.keymap.set("n", "L", "$", { noremap = true, silent = true })
 vim.keymap.set("n", "H", "^", { noremap = true, silent = true })
@@ -126,13 +28,10 @@ vim.keymap.set("i", "jk", "<Esc>", { noremap = true, silent = true })
 
 vim.cmd([[ command! -nargs=+ Grep execute 'silent grep! <args>' | copen ]])
 
-vim.cmd([[ cnoreabbrev <expr> bc 'lua MiniBufremove.delete()' ]])
+vim.g.mapleader = " "
 
-vim.keymap.set('n', '-', ':lua MiniFiles.open(vim.api.nvim_buf_get_name(0), false)<CR>', {})
-
-vim.keymap.set("n", "<leader>f",  ":Pick files<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>/",  ":Pick grep<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>b",  ":Pick buffers<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>f",  ":find ", { noremap = true })
+vim.keymap.set("n", "<leader>/",  ":Grep ", { noremap = true })
 
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -157,11 +56,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
     vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
     vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-    vim.keymap.set('n', '<space>wl', function()
-      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end, opts)
     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
     vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
     vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
